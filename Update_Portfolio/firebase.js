9cca9a4732189ebc6d7a755898541258699f7830
Awais Saddiqui <101595985+awaissaddiqui@users.noreply.github.com>
2025-05-19 16:30:59 +0500

    Update firebase.js

--- Update_Portfolio/firebase.js
+++ Update_Portfolio/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCPT5Ndhp7qQVWa5ZAP7rL63pOiOx9vc4I",
-    authDomain: "awais-saddiqui.firebaseapp.com",
-    projectId: "awais-saddiqui",
-    storageBucket: "awais-saddiqui.firebasestorage.app",
-    messagingSenderId: "781486998783",
-    appId: "1:781486998783:web:14236c3a2d5b29262bf8ca",
-    measurementId: "G-VZYKTZ9PNM"
+   
 };
 
 // Initialize Firebase
